d0a0f92614a08cd282e0997002a7074e14052b3e
Samelot <sizeoverload@gmail.com>
2017-08-12 12:44:12 -0400

    use python3

--- render-project-tracks.py
+++ render-project-tracks.py
@@ -1,3 +1,6 @@
+# use python3!!
+# python3 -m tabnanny -v render-project-tracks.py
+
 import sys
 import os
 import subprocess
@@ -8,7 +11,7 @@ if sys.platform == "win32":
     FFMPEG_BIN = "ffmpeg.exe"
     MOVE = "move "
     MKDIR = "md "
-    REAPER = "\Program Files\REAPER (x64)"
+    REAPER = r"C:\Program Files\REAPER (x64)\reaper.exe"
 elif sys.platform == 'linux' or sys.platform == 'linux2':
     FFMPEG_BIN = "ffmpeg"
     MOVE = "mv "
@@ -22,18 +25,33 @@ elif sys.platform == 'darwin':
 def render(projectsDir):
     # home = os.path.expanduser("~")
 
+    reaperProjectsArray = []
+
+    winPATH = r"C:\Users\Pixel Academy\Documents\Reaper\sam\sam.rpp"
+    macPATH = r"/Users/samenglander/_dev/RenderReaperProjects/Andorra/AndorraQuantizeFinal.rpp"
+
     if projectsDir:
-		projectsDir = projectsDir     	
+        projectsDir = projectsDir       
     else:
-    	projectsDir = os.path.dirname(os.path.realpath(__file__))
-   
+        projectsDir = os.path.dirname(os.path.realpath(__file__))
+
     target = projectsDir + "/*/*.RPP"
-    for f in glob.iglob(target):
-		print f
     
-    cmd = [REAPER, '-h']
+    for f in glob.iglob(target):
+    	reaperProjectsArray.append(f)
+
+    for i in range(len(reaperProjectsArray)):
+    	cmd = [REAPER, '-renderproject', reaperProjectsArray[i]]
+    	output = subprocess.check_output(cmd)
+
+    # print(len(reaperProjectsArray))
+
+    #cmd = [REAPER, '-h']
+    # cmd = [REAPER, '-renderproject', target]
+    #cmd = [REAPER, '-cfgfile', 'REAPER.ini', '-renderproject', winPATH]
+    
     #output = subprocess.check_output(cmd)
-    #output = subprocess.call(cmd)
+    # output = subprocess.call(cmd)
     
 def main():
     parser = argparse.ArgumentParser()
